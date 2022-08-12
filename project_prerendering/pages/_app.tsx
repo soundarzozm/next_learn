fa9118704fdc820d4f0de5620ffe57ed06971f51
Soundar Murugan <soundar.murugan1@learner.manipal.edu>
2022-08-12 22:06:24 +0530

    default Head

--- project_prerendering/pages/_app.tsx
+++ project_prerendering/pages/_app.tsx
@@ -2,9 +2,23 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout/layout'
 
+// NextJS takes the latest defined <title/> so the one defined here is more like default.
+import Head from 'next/head'
+
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<Layout>
+			<Head>
+				<title>Events</title>
+				<meta
+					name="description"
+					content="Events Project built with NextJS"
+				/>
+				<meta
+					name="viewport"
+					content="inital-scale=1.0, width=device-width"
+				/>
+			</Head>
 			<Component {...pageProps} />
 		</Layout>
 	)
