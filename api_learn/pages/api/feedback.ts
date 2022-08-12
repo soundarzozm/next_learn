944fdc1b9f65a53874375767a1ee9b6dccd83d17
Soundar Murugan <soundar.murugan1@learner.manipal.edu>
2022-08-12 23:43:05 +0530

    working post

--- api_learn/pages/api/feedback.ts
+++ api_learn/pages/api/feedback.ts
@@ -1,7 +1,38 @@
+import fs from 'fs'
+import path from 'path'
+
 function handler(req: any, res: any) {
-	res.status(200).json({
-		message: 'This works!'
-	})
+	if (req.method === 'POST') {
+		try {
+			const email = req.body.email
+			const feedbackText = req.body.text
+
+			const newFeedback = {
+				id: new Date().toISOString(),
+				email: email,
+				text: feedbackText,
+			}
+
+			const filePath = path.join(process.cwd(), 'data', 'feedback.json')
+			const fileData = fs.readFileSync(filePath)
+
+			const data = JSON.parse(fileData)
+
+			data.push(newFeedback)
+			fs.writeFileSync(filePath, JSON.stringify(data))
+
+			res.status(201).json({
+				message: 'Success!',
+				feedback: newFeedback,
+			})
+		} catch (error) {
+			console.log(error)
+		}
+	} else {
+		res.status(200).json({
+			message: 'This works!',
+		})
+	}
 }
 
 export default handler
--- api_learn/pages/index.tsx
+++ api_learn/pages/index.tsx
@@ -1,7 +1,41 @@
+import { useRef } from 'react'
+
 const HomePage = () => {
+	const emailInputRef = useRef()
+	const feedbackInputRef = useRef()
+
+	const handleSubmit = (event: any) => {
+		event.preventDefault() // Disable browser reload
+		const enteredEmail = emailInputRef.current.value
+		const enteredFeedback = feedbackInputRef.current.value
+
+		const reqBody = { email: enteredEmail, text: enteredFeedback }
+
+		fetch('/api/feedback', {
+			method: 'POST',
+			body: JSON.stringify(reqBody),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then((response) => response.json())
+			.then((data) => console.log(data))
+	}
+
 	return (
 		<div>
 			<h1>The Home Page</h1>
+			<form>
+				<div>
+					<label htmlFor="email">Your E-mail Address</label>
+					<input id="email" type="email" ref={emailInputRef} />
+				</div>
+				<div>
+					<label htmlFor="feedback">Your Feedback</label>
+					<textarea id="feedback" rows={5} ref={feedbackInputRef} />
+				</div>
+				<button onClick={handleSubmit}>Send Feedback</button>
+			</form>
 		</div>
 	)
 }
